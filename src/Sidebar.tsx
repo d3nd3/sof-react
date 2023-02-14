4e884bc25c05a598ec51e9f7b617965ba88dfb4e
d3nd3 <d3nd3second@gmail.com>
2023-02-14 21:02:03 +0000

    button fix

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -1,17 +1,24 @@
 import './css/SideBar.css'
-function Sidebar() {
-	return (
 
+import { useState } from "react";
+
+function Sidebar() {
+	const [hidden, setHidden] = useState(true);
+	let showMenu =
+		<ul>
+			<li>Server List</li>
+			<li>Maps</li>
+			<li>Skins</li>
+			<li>Mods</li>
+			<li>Contact</li>
+		</ul>;
+
+	let buttonLabel = hidden ? "Show Menu" : "Hide Menu";
+	return (
 		<div className="sidebar">
-			<ul>
-				<li>Server List</li>
-				<li>Maps</li>
-				<li>Skins</li>
-				<li>Mods</li>
-				<li>Contact</li>
-			</ul>
+			<button type="button" onClick={()=>setHidden(!hidden)}>{buttonLabel}</button>
+			{hidden || showMenu}
 		</div>
-	
 	);
 }
 
